7154bd21b1b73827af3985dc8ccaef3c1f5c2fe9
PhilippBR0 <68929021+PhilippBR0@users.noreply.github.com>
2023-10-05 16:08:03 -0300

    Update java15.java

--- exercícios/java15.java
+++ exercícios/java15.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
 public class java15 {
     public static void main(String[] args) {
-        
+  
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Informe um número:");
         int numero = scanner.nextInt();
 
-        int maiorDivisor = numero / 2;
-        
-        for (int i = maiorDivisor; i >= 1; i--) {
+        int maiorDivisor = 1;
+
+        for (int i = maiorDivisor; i <= numero; i++) {
             if (numero % i == 0) {
                 maiorDivisor = i;
-                break;
             }
         }
 
         System.out.println("O maior divisor do número é " + maiorDivisor);
 
-       
         scanner.close();
     }
 }
